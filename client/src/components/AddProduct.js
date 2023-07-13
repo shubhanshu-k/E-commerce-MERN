be912c6a4f66a821aa91c4ea8180531c08c78785
Shubhanshu Kakkar <kakkarhimanshu889@gmail.com>
2023-07-14 02:50:23 +0530

    added add product feature

--- client/src/components/AddProduct.js
+++ client/src/components/AddProduct.js
@@ -1,9 +1,38 @@
 import React from 'react';
 
 const AddProduct=()=>{
+ const [name,setName]=React.useState('');
+ const [price,setprice]=React.useState('');
+
+ const [category,setcategory]=React.useState('');
+
+ const [company,setcompany]=React.useState('');
+
+
+
     return (
-<div>
+<div className="form-control">
     <h1>Add Product</h1>
+    <input type="text" className="form-control-login" placeholder='Enter product name'
+    onChange={(e)=>{setName(e.target.value)}}    
+    />
+    <input type="text" className="form-control-login" placeholder='Enter product price'
+    
+    onChange={
+  (e)=>{setprice(e.target.value)}
+    }
+    
+    />
+    <input type="text" className="form-control-login" placeholder='Enter product category'
+    onChange={(e)=>{setcategory(e.target.value)}}
+    />
+    <input type="text"
+    className='form-control-login'
+    placeholder='Enter product company'/
+    onChange={(e)=>{setcompany(e.target.value)}}
+    >
+    <button type='button' className='login-button' >Add Product </button>
+
 </div>
 
     )
