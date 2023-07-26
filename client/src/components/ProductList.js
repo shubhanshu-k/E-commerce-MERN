aef3ffeacb51f67304bd7bce9c746f6dc4bc837f
Shubhanshu Kakkar <kakkarhimanshu889@gmail.com>
2023-07-27 04:44:37 +0530

    delete functionality added

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,35 +1,62 @@
-import React, { useEffect } from 'react';
-import { useState } from 'react';
-const ProductList=()=>{
-const[products,setProducts]=useState([]);
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { FaTrash } from "react-icons/fa";
 
-useEffect(()=>{
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
     getProduct();
-},[])
-const getProduct= async ()=>{
-let result = await fetch('http://localhost:5000/products');
-result=await result.json();
-setProducts(result);
-}
-console.warn(products);
-return (
-<div className="Product-list">
-    <h3>Product List</h3>
- 
-    {
-        products.map((item,index)=>
-            <ul>
-            <li>{index+1}</li>
-            <li>{item.name}</li>
-            <li>Rs.{item.price}</li>
-            <li>{item.category}</li>
-            <li>{item.company}</li>
-            </ul>
-        )
-    }   
-</div>
+  }, []);
+  const getProduct = async () => {
+    let result = await fetch("http://localhost:5000/products");
+    result = await result.json();
+    setProducts(result);
+  };
+  console.warn(products);
 
-)
-}
+  const deleteProduct = async (id) => {
+    let result = await fetch(`http://localhost:5000/product/${id}`, {
+      method: "Delete"
+    });
+    result = await result.json();
+    if (result) {
+      alert("Product deleted successfully");
+      getProduct();
+    }
+  };
+
+  return (
+    <div className="Product-list">
+      <h3>Product List</h3>
+      <ul>
+        <li>S. No.</li>
+        <li>Name</li>
+        <li>Price</li>
+        <li>category</li>
+        <li>company</li>
+        <li>Operation</li>
+      </ul>
+
+      {products.map((item, index) => (
+        <ul>
+          <li>{index + 1}</li>
+          <li>{item.name}</li>
+          <li>Rs.{item.price}</li>
+          <li>{item.category}</li>
+          <li>{item.company}</li>
+          <li>
+            <button
+              className="btn btn-danger"
+              onClick={() => deleteProduct(item._id)}
+            >
+              <FaTrash /> Delete
+            </button>
+          </li>
+        </ul>
+      ))}
+    </div>
+  );
+};
 
 export default ProductList;
--- server/index.js
+++ server/index.js
@@ -1,62 +1,56 @@
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
-const PORT = process.env.port || 5000
-const mongoose = require('mongoose')
-const Product=require("./db/Product");
+const PORT = process.env.port || 5000;
+const mongoose = require("mongoose");
+const Product = require("./db/Product");
 const User = require("./db/user");
-require('dotenv').config()
+require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use(cors());
 mongoose
-.connect(process.env.MONGODB_URL)
-.then(()=>console.log(`connected To MongoDB...`))
-.catch((err)=>console.log(err))
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log(`connected To MongoDB...`))
+  .catch((err) => console.log(err));
 
-app.post("/add-product",async (req,resp)=>
-{
- let product=new Product(req.body);
- let result = await product.save();
- resp.send(result);
-})
+app.post("/add-product", async (req, resp) => {
+  let product = new Product(req.body);
+  let result = await product.save();
+  resp.send(result);
+});
 
 app.post("/register", async (req, resp) => {
   let user = new User(req.body);
   let result = await user.save();
   result = result.toObject();
-  delete result.password
-  resp.send(req.body)
-})
+  delete result.password;
+  resp.send(req.body);
+});
 app.post("/login", async (req, resp) => {
-  console.log(req.body)
+  console.log(req.body);
   if (req.body.password && req.body.email) {
     let user = await User.findOne(req.body).select("-password");
     if (user) {
-      resp.send(user)
+      resp.send(user);
+    } else {
+      resp.send({ result: "No User Found" });
     }
-    else {
-      resp.send({ result: "No User Found" })
-    }
+  } else {
+    resp.send({ result: "No User Found" });
   }
-  else {
-    resp.send({ result: 'No User Found' })
+});
+app.get("/products", async (req, resp) => {
+  const products = await Product.find();
+  if (products.length > 0) {
+    resp.send(products);
+  } else {
+    resp.send({ result: "No Product Found" });
   }
-})
-app.get("/products",async (req,resp)=>{
-  const products=await Product.find();
-  if(products.length>0){
-    resp.send(products)
-  }
-  else{
-    resp.send({result:"No Product Found"})
-  }
+});
 
-})
+app.delete("/product/:id", async (req, resp) => {
+  const result = await Product.deleteOne({ _id: req.params.id })
+  resp.send(result);
+});
 
-
-
-
-
-
-
-app.listen(PORT,()=>console.log(`Listening on: ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on: ${PORT}`));
