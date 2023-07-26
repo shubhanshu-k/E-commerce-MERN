5d6cae0f1c25600968901eb48db55baaf87e2a11
Shubhanshu Kakkar <kakkarhimanshu889@gmail.com>
2023-07-27 03:13:10 +0530

    fixed some bugs in add product feature

--- client/src/components/AddProduct.js
+++ client/src/components/AddProduct.js
@@ -1,66 +1,87 @@
-import React from 'react';
+import React from "react";
 
-const AddProduct=()=>{
- const [name,setName]=React.useState('');
- const [price,setprice]=React.useState('');
+const AddProduct = () => {
+  const [name, setName] = React.useState("");
+  const [price, setprice] = React.useState("");
 
- const [category,setcategory]=React.useState('');
+  const [category, setcategory] = React.useState("");
 
- const [company,setcompany]=React.useState('');
-const [error,setError]=React.useState(false);
-const addProduct=async()=>{
+  const [company, setcompany] = React.useState("");
+  const [error, setError] = React.useState(false);
 
-if(!name || !price || !company ||category){
-    setError(true);
-    return false;
-
-}
-
- console.warn(name,price,category,company);
- let result =await fetch("http://localhost:5000/add-product",{
-    method:"post",
-    body:JSON.stringify({name,price,category,company}),
-    headers:{
-        "Content-type":"application/json"
+  const addProduct = async () => {
+    if (!name || !price || !company || !category) {
+      setError(true);
+      return false;
     }
- });
- result = await result.json();   
- console.log(result);
-}
 
-    return (
-<div className="form-control">
-    <h1>Add Product</h1>
-    <input type="text" className="form-control-login" placeholder='Enter product name'
-    onChange={(e)=>{setName(e.target.value)}}    
-    />
-{ error && !name&&<span className="invalid-input">Enter Valid Name </span>}
+    console.warn(name, price, category, company);
+    let result = await fetch("http://localhost:5000/add-product", {
+      method: "post",
+      body: JSON.stringify({ name, price, category, company }),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    result = await result.json();
+    console.warn(result);
+    alert("Product Added Successfully");
+  };
 
-    <input type="text" className="form-control-login" placeholder='Enter product price'
-    
-    onChange={
-  (e)=>{setprice(e.target.value)}
-    }   
-    />
-    { error && !price &&<span className="invalid-input">Enter Valid Price </span>}
+  return (
+    <div className="form-control">
+      <h1>Add Product</h1>
+      <input
+        type="text"
+        className="form-control-login"
+        placeholder="Enter product name"
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      {error && !name && (
+        <span className="invalid-input">Enter Valid Name </span>
+      )}
 
+      <input
+        type="text"
+        className="form-control-login"
+        placeholder="Enter product price"
+        onChange={(e) => {
+          setprice(e.target.value);
+        }}
+      />
+      {error && !price && (
+        <span className="invalid-input">Enter Valid Price </span>
+      )}
 
-    <input type="text" className="form-control-login" placeholder='Enter product category'
-    onChange={(e)=>{setcategory(e.target.value)}}
-    />
-{ error && !category&&<span className="invalid-input">Enter Valid category </span>}
+      <input
+        type="text"
+        className="form-control-login"
+        placeholder="Enter product category"
+        onChange={(e) => {
+          setcategory(e.target.value);
+        }}
+      />
+      {error && !category && (
+        <span className="invalid-input">Enter Valid category </span>
+      )}
 
+      <input
+        type="text"
+        className="form-control-login"
+        placeholder="Enter product company"
+        onChange={(e) => {
+          setcompany(e.target.value);
+        }}
+      />
+      {error && !company && (
+        <span className="invalid-input">Enter Valid company </span>
+      )}
 
-
-    <input type="text"
-    className='form-control-login'
-    placeholder='Enter product company'
-    onChange={(e)=>{setcompany(e.target.value)}}
-/>
-{ error && !company&&<span className="invalid-input">Enter Valid company </span>}
-    <button type='button' onClick={addProduct} className='login-button' >Add Product </button>
-
-</div>
-    )
-}
+      <button type="button" onClick={addProduct} className="login-button">AddProduct
+      </button>
+    </div>
+  );
+};
 export default AddProduct;
